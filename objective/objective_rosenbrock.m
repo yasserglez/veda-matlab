167a8c92a51ed3460901e66fe5ce379aa69b2f12
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-04-18 23:55:53 -0400

    Adding the Rosenbrock objective function.

---
+++ objective/objective_rosenbrock.m
@@ -0,0 +1,21 @@
+function [evaluation] = objective_rosenbrock(params, population)
+  % Evaluates the Rosenbrock objective function. 
+  %
+  % PARAMS is a struct with the parameters of the EDA and POPULATION the
+  % population matrix. The output variable EVALUATION is a column vector with
+  % the evaluation of the function at each individual of the given population.
+  %
+  % Search domain: -5 <= xi <= 10, i = 1, ..., n. 
+  % Global minima: x* = (1, ..., 1), f(x*) = 0.
+
+  x = population;
+  n = size(population, 2);
+  m = size(population, 1);
+  z = zeros(m, 1);
+  for i = 1:m
+    for j = 1:n - 1
+      z(i) = z(i) + 100 * (x(i,j)^2 - x(i,j + 1))^2 + (x(i,j) - 1)^2;
+    end
+  end
+  evaluation = z;
+end
--- objective/objective_sphere.m
+++ objective/objective_sphere.m
@@ -4,6 +4,9 @@ function [evaluation] = objective_sphere(params, population)
   % PARAMS is a struct with the parameters of the EDA and POPULATION the
   % population matrix. The output variable EVALUATION is a column vector with
   % the evaluation of the function at each individual of the given population.
+  %
+  % Search domain: −5.12 <= xi <= 5.12, i = 1, ..., n.
+  % The global minima: x* = (0, ..., 0), f(x*) = 0.
 
   evaluation =  sum(population .^ 2, 2);
 end
