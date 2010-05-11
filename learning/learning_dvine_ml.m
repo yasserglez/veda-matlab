855c51e9d18ab24446756afa1f098ece9a831958
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-05-10 23:39:47 -0400

    Supporting setting a maximum number of trees in the D-vine.

--- learning/learning_dvine_ml.m
+++ learning/learning_dvine_ml.m
@@ -5,10 +5,17 @@ function model = learning_dvine_ml(params, population, fitness)
   % population from where the probabilistic model should be learned and
   % FITNESS is the fitness of the individuals of this popualtion.
   %
-  % A D-vine is defined by an ordering of the variables and the parameters of
+  % A D-vine is defined by an order of the variables and the parameters of
   % each copula density in the factorization. The output variable MODEL will be
-  % an struct with two fields: ordering, a vector for the order of the
-  % variables and parameters, a cell array with the parameters of each copula.
+  % an struct with the following fields:
+  %
+  %   * order: vector with the order of the variables,
+  %   * theta: cell array with the parameters of each bivariate copula in the
+  %     pair-copula decomposition,
+  %   * h_functions: cell array with the h-functions corresponding to each
+  %     bivariate copula in the pair-copula decomposition,
+  %   * h_inverses: cell array with the inverse of the h-functions corresponding
+  %     to each bivariate copula in the pair-copula decomposition.
   %
   % References:
   %
@@ -17,84 +24,111 @@ function model = learning_dvine_ml(params, population, fitness)
   
   % Created by Yasser González Fernández (2010).  
   
-  num_individuals = size(population, 1);
+  pop_size = size(population, 1);
   num_vars = size(population, 2);
   
   % Select an ordering of the variables.
   if params.learning_params.random_ordering
-    ordering = randperm(num_vars);
+    order = randperm(num_vars);
   else
-    ordering = 1:num_vars;
+    order = 1:num_vars;
   end
   
   % Transform the population into an Uniform(0,1) population applying the
   % univariate marginal CDFs to each observation of each variable in the
   % population.
-  uniform_pop = zeros(num_individuals, num_vars);
-  marginal_cdf = params.learning_params.marginal_cdf;
+  uniform_pop = zeros(pop_size, num_vars);
   for k = 1:num_vars
-    uniform_pop(:,k) = feval(marginal_cdf, ...
-                             population(:,ordering(k)), ...
-                             population(:,ordering(k)));
+    uniform_pop(:,k) = feval(params.learning_params.marginal_cdf, ...
+                             population(:,order(k)), population(:,order(k)));
   end
   
-  % Calculate the starting values for the parameters of the copulas needed for
-  % the numerical maximization of the log-likelihood function.
-  copula_fit = params.learning_params.copula_fit;
-  h_function = params.learning_params.h_function;
-  parameters = starting_parameters(uniform_pop, copula_fit, h_function);
+  % Calculate the starting values for the parameters of the copulas needed
+  % for the numerical maximization of the log-likelihood function.
+  [theta, h_functions, h_inverses] = starting_theta(params, uniform_pop);
   
   % Run a local optimization method for the log-likehood.
   
   % Set the information of the model.
   model = struct();
-  model.ordering = ordering;
-  model.parameters = parameters;
+  model.order = order;
+  model.theta = theta;
+  model.h_functions = h_functions;
+  model.h_inverses = h_inverses;
 end
 
-function parameters = starting_parameters(uniform_pop, copula_fit, h_function)
+function [theta, h_functions, h_inverses] = starting_theta(params, uniform_pop)
   % Calculate starting parameters of the copulas in a D-vine.
   
   % WARNING: The way the first dimension of v is indexed here differs from the
   % Algorithm 4 in SAMBA/24/06 because of MATLAB 1-based indexing.
   
+  copulafit = params.learning_params.copulafit;  
+  h_function = params.learning_params.h_function;
+  h_inverse = params.learning_params.h_inverse;
+  if isfield(params.learning_params, 'max_trees')
+    max_trees = params.learning_params.max_trees;
+  else
+    max_trees = NaN;
+  end
+  
   n = size(uniform_pop, 2);
+
+  theta = cell(n, n);
+  h_functions = cell(n, n);
+  h_inverses = cell(n, n);  
+  
   v = cell(n, n);
-  parameters = cell(n, n);
 
   for i = 1:n
     v{1,i} = uniform_pop(:,i);
   end
   
   for i = 1:n-1
-    parameters{1,i} = feval(copula_fit, v{1,i}, v{1,i+1});
+    if isnan(max_trees) || 1 <= max_trees
+      theta{1,i} = feval(copulafit, v{1,i}, v{1,i+1});
+      h_functions{1,i} = str2func(h_function);
+      h_inverses{1,i} = str2func(h_inverse);
+    else
+      theta{1,i} = [];
+      h_functions{1,i} = str2func('h_product');
+      h_inverses{1,i} = str2func('hinv_product');
+    end
   end
-  v{2,1} = feval(h_function, v{1,1}, v{1,2}, parameters{1,1});
+  v{2,1} = feval(h_functions{1,1}, v{1,1}, v{1,2}, theta{1,1});
   
   for k = 1:n-3
-    v{2,2*k} = feval(h_function, v{1,k+2}, v{1,k+1}, parameters{1,k+1});
-    v{2,2*k+1} = feval(h_function, v{1,k+1}, v{1,k+2}, parameters{1,k+1});
+    v{2,2*k} = feval(h_functions{1,k+1}, v{1,k+2}, v{1,k+1}, theta{1,k+1});
+    v{2,2*k+1} = feval(h_functions{1,k+1}, v{1,k+1}, v{1,k+2}, theta{1,k+1});
   end
-  v{2,2*n-4} = feval(h_function, v{1,n}, v{1,n-1}, parameters{1,n-1});
+  v{2,2*n-4} = feval(h_functions{1,n-1}, v{1,n}, v{1,n-1}, theta{1,n-1});
                            
   for j = 2:n-1
     for i = 1:n-j
-      parameters{j,i} = feval(copula_fit, v{j,2*i-1}, v{j,2*i});
+      if isnan(max_trees) || j <= max_trees
+        theta{j,i} = feval(copulafit, v{j,2*i-1}, v{j,2*i});
+        h_functions{j,i} = str2func(h_function);
+        h_inverses{j,i} = str2func(h_inverse);
+      else
+        theta{j,i} = [];
+        h_functions{j,i} = str2func('h_product');
+        h_inverses{j,i} = str2func('hinv_product');
+      end
     end
     
     % Compute observations for the next tree.
     if j ~= n-1
-      v{j+1,1} = feval(h_function, v{j,1}, v{j,2}, parameters{j,1});
+      v{j+1,1} = feval(h_functions{j,1}, v{j,1}, v{j,2}, theta{j,1});
       if n > 4
         for i = 1:n-j-2
-          v{j+1,2*i} = feval(h_function, v{j,2*i+2}, v{j,2*i+1}, ...
-                             parameters{j,i+1});
-          v{j+1,2*i+1} = feval(h_function, v{j,2*i+1}, v{j,2*i+2}, ...
-                               parameters{j,i+1});
+          v{j+1,2*i} = feval(h_functions{j,i+1}, v{j,2*i+2}, ...
+                             v{j,2*i+1}, theta{j,i+1});
+          v{j+1,2*i+1} = feval(h_functions{j,i+1}, v{j,2*i+1}, ...
+                               v{j,2*i+2}, theta{j,i+1});
         end
       end
-      v{j+1,2*n-2*j-2} = feval(h_function, v{j,2*n-2*j}, v{j,2*n-2*j-1}, ...
-                               parameters{j,n-j});
+      v{j+1,2*n-2*j-2} = feval(h_functions{j,n-j}, v{j,2*n-2*j}, ...
+                               v{j,2*n-2*j-1}, theta{j,n-j});
     end
   end
 end
--- sampling/sampling_dvine.m
+++ sampling/sampling_dvine.m
@@ -3,9 +3,8 @@ function population = ...
   % Sample a D-vine learned from a population.
   %
   % PARAMS is a struct with the parameters of the EDA. MODEL is a struct
-  % representing the D-vine, it should have two fields: ordering, a vector for
-  % the order of the variables and parameters, a matrix with  the parameters
-  % of each copula in the pair-copula decomposition. SELECTED_POPULATION is the
+  % representing the D-vine, see the documentation of the learning_dvine_ml
+  % function for information about the fields. SELECTED_POPULATION is the
   % population from where the canonical vine was learned and SELECTED_FITNESS
   % its evaluation.
   %
@@ -27,9 +26,12 @@ function population = ...
     
   pop_size = params.seeding_params.population_size;
   n = params.objective_params.number_variables;
-  h_inverse = params.sampling_params.h_inverse;
-  h_function = params.sampling_params.h_function;
-  parameters = model.parameters;
+  cdf_inverse = params.sampling_params.marginal_cdf_inverse;
+  
+  order = model.order;
+  theta = model.theta;
+  h_functions = model.h_functions;
+  h_inverses = model.h_inverses;
 
   uniform_pop = zeros(pop_size, n);
   W = unifrnd(0, 1, pop_size, n);
@@ -39,33 +41,33 @@ function population = ...
     w = W(individual,:);
     v(1,1) = w(1);
     uniform_pop(individual,1) = v(1,1);   
-    v(2,1) = feval(h_inverse, w(2), v(1,1), parameters{1,1});
+    v(2,1) = feval(h_inverses{1,1}, w(2), v(1,1), theta{1,1});
     uniform_pop(individual,2) = v(2,1);
-    v(2,2) = feval(h_function, v(1,1), v(2,1), parameters{1,1});
+    v(2,2) = feval(h_functions{1,1}, v(1,1), v(2,1), theta{1,1});
     
     for i = 3:n
       % Sampling the ith variable.
       v(i,1) = w(i);
       for k = i-1:-1:2
-        v(i,1) = feval(h_inverse, v(i,1), v(i-1,2*k-2), parameters{k,i-k});
+        v(i,1) = feval(h_inverses{k,i-k}, v(i,1), v(i-1,2*k-2), theta{k,i-k});
       end
-      v(i,1) = feval(h_inverse, v(i,1), v(i-1,1), parameters{1,i-1});
+      v(i,1) = feval(h_inverses{1,i-1}, v(i,1), v(i-1,1), theta{1,i-1});
       uniform_pop(individual,i) = v(i,1);
       
       % Compute the h-inverse values needed for sampling the (i+1)th variable.
       if i ~= n
-        v(i,2) = feval(h_function, v(i-1,1), v(i,1), parameters{1,i-1});
-        v(i,3) = feval(h_function, v(i,1), v(i-1,1), parameters{1,i-1});
+        v(i,2) = feval(h_functions{1,i-1}, v(i-1,1), v(i,1), theta{1,i-1});
+        v(i,3) = feval(h_functions{1,i-1}, v(i,1), v(i-1,1), theta{1,i-1});
         if i > 3
           for j = 2:i-2
-            v(i,2*j) = feval(h_function, v(i-1,2*j-2), v(i,2*j-1), ...
-                             parameters{j,i-j});
-            v(i,2*j+1) = feval(h_function, v(i,2*j-1), v(i-1,2*j-2), ...
-                               parameters{j,i-j});
+            v(i,2*j) = feval(h_functions{j,i-j}, v(i-1,2*j-2), ...
+                             v(i,2*j-1), theta{j,i-j});
+            v(i,2*j+1) = feval(h_functions{j,i-j}, v(i,2*j-1), ...
+                               v(i-1,2*j-2), theta{j,i-j});
           end
         end
-        v(i,2*i-2) = feval(h_function, v(i-1,2*i-4), v(i,2*i-3), ...
-                           parameters{i-1,1});
+        v(i,2*i-2) = feval(h_functions{i-1,1}, v(i-1,2*i-4), ...
+                           v(i,2*i-3), theta{i-1,1});
       end
     end
   end
@@ -74,13 +76,9 @@ function population = ...
   % variables of the objective function applying the inverse of the univariate
   % marginal CDFs to each observation of each variable in the population.
   
-  cdf_inverse = params.sampling_params.marginal_cdf_inverse;
-  ordering = model.ordering;
-  
   population = zeros(pop_size, n);
   for k = 1:n
-    population(:,ordering(k)) = feval(cdf_inverse, ...
-                                      selected_population(:,ordering(k)), ...
-                                      uniform_pop(:,k));
+    population(:,order(k)) = ...
+      feval(cdf_inverse, selected_population(:,order(k)), uniform_pop(:,k));
   end
 end
