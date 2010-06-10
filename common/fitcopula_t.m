ee87ec1c0ad372a7bbb5f733268c53cc8f874c07
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-06-10 00:57:25 -0400

    Implementing fitting the t copula.

---
+++ common/fitcopula_t.m
@@ -0,0 +1,20 @@
+function theta = fitcopula_t(x, y)
+  % Estimate the parameters of a bivariate t copula.
+  %
+  % This function is used to estimate the parameters of a bivariate copula in a
+  % pair-copula decomposition of a multivariate density function. X and Y are
+  % samples of correlated Uniform(0,1) variables. The output variable
+  % is a vector with the estimated parameters of the copula.
+  
+  % Created by Yasser González Fernández (2010).
+  
+  % TODO: Modify to support running in Octave.
+  
+  x(x <= 0) = 0 + eps;
+  x(x >= 1) = 1 - eps;
+  y(y <= 0) = 0 + eps;
+  y(y >= 1) = 1 - eps;
+  
+  [sigma, dof] = copulafit('t', [x y], 'Method', 'ApproximateML');
+  theta = [sigma(1,2), dof];
+end
