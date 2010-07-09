652f1104e1b8a3c3ede2f769e9dcbde7c4084103
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-07-09 11:26:44 -0400

    Avoid NaN values in the copulas GoF tests.

--- common/gof_gaussian.m
+++ common/gof_gaussian.m
@@ -1,5 +1,12 @@
 function result = gof_gaussian(x, y)
   % Goodness-of-fit test for the bivariate Gaussian copula.
   
+  % Created by Yasser González Fernández (2010).  
+  
+  x(x <= 0) = 0 + eps;
+  x(x >= 1) = 1 - eps;
+  y(y <= 0) = 0 + eps;
+  y(y >= 1) = 1 - eps;  
+  
   result = callR('gofCopulaWrapper', 'gaussian', [x, y]);
 end
