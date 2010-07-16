418a2af52a2d8bff09dbdfa66f93559b410bc4e9
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-07-16 01:20:32 -0400

    Using MPL instead of inversion of tau in copula GoF tests.

--- R/scripts/gofCopulaWrapper.R
+++ R/scripts/gofCopulaWrapper.R
@@ -2,5 +2,5 @@ gofCopulaWrapper <- function(copula, data) {
     c <- switch(copula, 
                 frank=frankCopula(1),
                 gaussian=normalCopula(0))
-    gofCopula(c, data, method="itau", simulation="mult")
+    gofCopula(c, data, method="mpl", simulation="mult")
 }
