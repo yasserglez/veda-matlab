a603b9197d6b834d5dbe69a49d470cc279972f2e
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-07-25 22:20:21 -0400

    Implementing h-functions for the Gumbel copula.

--- R/scripts/gofCopulaWrapper.R
+++ R/scripts/gofCopulaWrapper.R
@@ -13,14 +13,20 @@ gofCopulaInternal <- function(copulaName, data) {
             frankCopula(1)
         }
         else if (copulaName == "clayton") {
-            claytonCopula(1)
+            c <- claytonCopula(1)
         }
+        else if (copulaName == "gumbel") {
+            gumbelCopula(1)
+        }        
         else if (copulaName == "gaussian") {
             normalCopula(0)
         }        
         else if (copulaName == "t") {
-            tcopula <- tCopula(0)
-            fit <- fitCopula(tcopula, data, method="ml", start=tcopula@parameters)
+            # The gofCopula method requires the degrees-of-freedom parameter to be fixed.
+            # Therefore, we estimate a value for this parameters before creating the 
+            # copula object.
+            c <- tCopula(0)
+            fit <- fitCopula(c, data, method="ml", start=c@parameters)
             rho <- fit@copula@parameters[1]
             df <- fit@copula@parameters[2]
             tCopula(rho, df=df, df.fixed=TRUE)
@@ -28,7 +34,15 @@ gofCopulaInternal <- function(copulaName, data) {
     }
     result <- gofCopula(copula, data, method="itau", simulation="mult")
     if (copulaName == "t") {
+        # As the degrees-of-freedom is fixed it is not included in the parameters.
         result$parameters <- c(result$parameters, copula@df)
+    }
+    else if (copulaName == "clayton") {
+        # Lower bound of the parameters set to -1 for the bivariate Clayton copula
+        # wich differs with the Appendix B.3 of Aas, K., Czado, C., Frigessi, A. & Bakken,
+        # H. Pair-copula constructions of multiple dependence Norwegian Computing Center, 
+        # NR, 2006 (SAMBA/24/06).
+        result$parameters <- max(result$parameters, 0)
     }
     result
 }
---
+++ common/h_gumbel.m
@@ -0,0 +1,20 @@
+function results = h_gumbel(u1, u2, delta)
+  % Evaluate the h-function of the bivariate Gumbel copula.
+  %
+  % References:
+  %
+  % K. Aas, C. Czado, A. Frigessi, and H. Bakken. Pair-copula constructions of
+  % multiple dependence. Note SAMBA/24/06, Norwegian Computing Center, NR, 2006.  
+
+  % Created by Yasser González Fernández (2010).
+
+  u1(u1 <= 0) = 0 + eps;
+  u1(u1 >= 1) = 1 - eps;
+  u2(u2 <= 0) = 0 + eps;
+  u2(u2 >= 1) = 1 - eps;
+
+  C = exp(-((((-log(u1)) .^ delta) + ((-log(u2)) .^ delta)) .^ (1/delta)));
+
+  results = C .* (1./u2) .* ((-log(u2)) .^ (delta-1)) ...
+    .* ((((-log(u1)) .^ delta) + ((-log(u2)) .^ delta)) .^ (1/(delta-1)));
+end
