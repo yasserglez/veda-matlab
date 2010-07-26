2636fd4c91158dbd9053f1e1d7254bd9ae52f309
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-07-26 01:37:50 -0400

    Removing fixing the paramter of the Clayton copula in gofCopulaWrapper.

--- R/scripts/gofCopulaWrapper.R
+++ R/scripts/gofCopulaWrapper.R
@@ -1,5 +1,5 @@
 gofCopulaWrapper <- function(copulaName, data) {
-    result <- tryCatch(gofCopulaInternal(copulaName, data), error = function(error) NULL)
+    result <- tryCatch(gofCopulaInternal(copulaName, data), error=function(error) NULL)
     if (is.null(result)) {
         # Return a value that will make the algorithm ignore this copula.
         result <- list(statistic=.Machine$double.xmax, pvalue=0, parameters=0)
@@ -13,14 +13,14 @@ gofCopulaInternal <- function(copulaName, data) {
             frankCopula(1)
         }
         else if (copulaName == "clayton") {
-            c <- claytonCopula(1)
+            claytonCopula(1)
         }
         else if (copulaName == "gumbel") {
             gumbelCopula(1)
         }        
         else if (copulaName == "gaussian") {
             normalCopula(0)
-        }        
+        }
         else if (copulaName == "t") {
             # The gofCopula method requires the degrees-of-freedom parameter to be fixed.
             # Therefore, we estimate a value for this parameters before creating the 
@@ -36,13 +36,6 @@ gofCopulaInternal <- function(copulaName, data) {
     if (copulaName == "t") {
         # As the degrees-of-freedom is fixed it is not included in the parameters.
         result$parameters <- c(result$parameters, copula@df)
-    }
-    else if (copulaName == "clayton") {
-        # Lower bound of the parameters set to -1 for the bivariate Clayton copula
-        # wich differs with the Appendix B.3 of Aas, K., Czado, C., Frigessi, A. & Bakken,
-        # H. Pair-copula constructions of multiple dependence Norwegian Computing Center, 
-        # NR, 2006 (SAMBA/24/06).
-        result$parameters <- max(result$parameters, 0)
     }
     result
 }
