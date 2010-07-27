60d091e6d11e460ec7e5e918e41121d0945844da
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-07-27 02:43:47 -0400

    Using the MATLAB functions for kendall_corr_test.

--- common/kendall_corr_test.m
+++ common/kendall_corr_test.m
@@ -1,11 +1,13 @@
 function h = kendall_corr_test(x, y, alpha)
   % Test whether two samples come from uncorrelated populations.
   
-  n = numel(x);
-  tau = kendall_corr(x, y);
-  stat = tau / sqrt((2 * (2*n+5)) / (9*n*(n-1)));
-  cdf = normcdf(stat, 0, 1);
-  p = 2 * min(cdf, 1-cdf);
+  if exist('kendall', 'file') == 2
+    % Octave Statistics Package.
+    p = corr_test(x, y, '!=', 'kendall');
+  else
+    % MATLAB Statistics Toolbox.
+    [tau, p] = corr(x, y, 'type', 'Kendall');
+  end
 
   h = (p <= alpha);
 end
